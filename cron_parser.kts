88d61fc7494ba98ee258bae56a73a75d94cce65c
raxigan <raxigan@gmail.com>
2023-05-05 13:35:00 +0200

    Fix

--- cron_parser.kts
+++ cron_parser.kts
@@ -1,12 +1,14 @@
 #!/usr/bin/env kscript
 
+@file:Import("src/main/kotlin/com/homework/App.kt")
 @file:Import("src/main/kotlin/com/homework/CronParser.kt")
 @file:Import("src/main/kotlin/com/homework/FieldDescriptor.kt")
 @file:Import("src/main/kotlin/com/homework/FieldParser.kt")
 
 // kscript <4.0.0
+//@file:Include("src/main/kotlin/com/homework/App.kt")
 //@file:Include("src/main/kotlin/com/homework/CronParser.kt")
 //@file:Include("src/main/kotlin/com/homework/FieldDescriptor.kt")
 //@file:Include("src/main/kotlin/com/homework/FieldParser.kt")
 
-parse(args)
+main(args)
--- src/main/kotlin/com/homework/App.kt
+++ src/main/kotlin/com/homework/App.kt
@@ -1,10 +1,5 @@
 package com.homework
 
-class App {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            parse(args)
-        }
-    }
+fun main(args: Array<String>) {
+    parse(args)
 }
